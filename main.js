63279e1ff00bef334c653655d89ec3c09dd2d6da
Edwin Perez <edwinperez539@gmail.com>
2016-08-08 16:09:18 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+// TODO: create a container
+
+// TODO: create a ticker component
+
+// TODO: create a store
+
+// TODO: create an action
+
+// TODO: create a reducer
+
+render(<h1>Hello world</h1>, document.getElementById('container'));
